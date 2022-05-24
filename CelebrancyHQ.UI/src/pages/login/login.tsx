c18dec7c6502041e14bf20ee24c8578ed42c6f12
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-05-24 16:40:54 +1000

    CHQ-2: Added email address and password fields to login page.

--- CelebrancyHQ.UI/src/pages/login/login.tsx
+++ CelebrancyHQ.UI/src/pages/login/login.tsx
@@ -9,7 +9,33 @@ export class Login extends React.Component<LoginProps, LoginState> {
         return (
             <main>
                 <h1>Login</h1>
-                <div>Insert login page content here</div>
+                <form id="login-form">
+                    <div className="container-fluid p-0">
+                        <div className="row form-group">
+                            <div className="col-2">
+                                <label htmlFor="email-address" className="col-form-label">Email address:</label>
+                            </div>
+                            <div className="col-10">
+                                <input id="email-address" type="email" className="form-control" />
+                            </div>
+                        </div>
+                        <div className="row form-group">
+                            <div className="col-2">
+                                <label htmlFor="password" className="col-form-label">Password:</label>
+                            </div>
+                            <div className="col-10">
+                                <input id="password" type="password" className="form-control" />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-2">
+                            </div>
+                            <div className="col-10">
+                                <button type="submit" className="btn btn-primary">Login</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </main>
         );
     }
