37f6785e0aedf815906445ef6a27ba89b1a35ab7
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-05-24 17:02:02 +1000

    CHQ-1: Made login page mobile ready.

--- CelebrancyHQ.UI/src/pages/login/login.tsx
+++ CelebrancyHQ.UI/src/pages/login/login.tsx
@@ -3,6 +3,19 @@ import React from "react";
 export class Login extends React.Component<LoginProps, LoginState> {
     constructor(props: LoginProps) {
         super(props);
+
+        this.state = {
+            emailAddress: '',
+            password: ''
+        };
+    }
+
+    setEmail(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ emailAddress: event.target.value });
+    }
+
+    setPassword(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ password: event.target.value });
     }
 
     render() {
@@ -12,25 +25,25 @@ export class Login extends React.Component<LoginProps, LoginState> {
                 <form id="login-form">
                     <div className="container-fluid p-0">
                         <div className="row form-group">
-                            <div className="col-2">
+                            <div className="col-lg-2 col-sm-3 col-12">
                                 <label htmlFor="email-address" className="col-form-label">Email address:</label>
                             </div>
-                            <div className="col-10">
-                                <input id="email-address" type="email" className="form-control" />
+                            <div className="col-lg-10 col-sm-9 col-12">
+                                {this.state.emailAddress}
+                                <input id="email-address" type="email" className="form-control" value={this.state.emailAddress} onChange={this.setEmail.bind(this)} />
                             </div>
                         </div>
                         <div className="row form-group">
-                            <div className="col-2">
+                            <div className="col-lg-2 col-sm-3 col-12">
                                 <label htmlFor="password" className="col-form-label">Password:</label>
                             </div>
-                            <div className="col-10">
-                                <input id="password" type="password" className="form-control" />
+                            <div className="col-lg-10 col-sm-9 col-12">
+                                {this.state.password}
+                                <input id="password" type="password" className="form-control" value={this.state.password} onChange={this.setPassword.bind(this)} />
                             </div>
                         </div>
                         <div className="row">
-                            <div className="col-2">
-                            </div>
-                            <div className="col-10">
+                            <div className="col-lg-10 col-sm-9 col-12 offset-lg-2 offset-sm-3">
                                 <button type="submit" className="btn btn-primary">Login</button>
                             </div>
                         </div>
@@ -46,5 +59,6 @@ interface LoginProps {
 }
 
 interface LoginState {
-
+    emailAddress: string;
+    password: string;
 }
