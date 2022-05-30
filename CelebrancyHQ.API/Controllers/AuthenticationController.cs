371f25613ecb75dfe1ad92f4a40ee9227b1567c8
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-05-30 13:38:14 +1000

    CHQ-1: Added AuthenticationController class.

---
+++ CelebrancyHQ.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using CelebrancyHQ.Models.DTOs.Authentication;
+using CelebrancyHQ.Models.DTOs.Users;
+
+namespace CelebrancyHQ.API.Controllers
+{
+    /// <summary>
+    /// Provides CelebrancyHQ authentication functionality.
+    /// </summary>
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        /// <summary>
+        /// Logs into CelebrancyHQ.
+        /// </summary>
+        /// <param name="loginDetails">The login details of the user.</param>
+        /// <returns>The details of the user who has logged in.</returns>
+        [HttpPost]
+        public async Task<ActionResult<UserDTO>> Login(LoginDetailsDTO loginDetails)
+        {
+            // TODO: Check the database here.
+            // TODO: Check for invalid user details.
+            return new UserDTO()
+            {
+                Id = 1,
+                FirstName = "Tahlee-Joy",
+                LastName = "Grace",
+                BusinessName = "Q Celebrancy",
+                EmailAddress = "tahlee.grace@gmail.com"
+            };
+        }
+    }
+}
