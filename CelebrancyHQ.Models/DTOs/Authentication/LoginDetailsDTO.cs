d4db65e615e8aa6e11490cec78fcf88d5e3ecf33
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-05-30 13:27:37 +1000

    CHQ-1: Added LoginDetailsDTO and UserDTO classes.

---
+++ CelebrancyHQ.Models/DTOs/Authentication/LoginDetailsDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace CelebrancyHQ.Models.DTOs.Authentication
+{
+    /// <summary>
+    /// Stores an email address and password for logging in to CelebrancyHQ.
+    /// </summary>
+    public class LoginDetailsDTO
+    {
+        public string EmailAddress { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ CelebrancyHQ.Models/DTOs/Users/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace CelebrancyHQ.Models.DTOs.Users
+{
+    /// <summary>
+    /// Stores details about a user.
+    /// </summary>
+    public class UserDTO
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string BusinessName { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
