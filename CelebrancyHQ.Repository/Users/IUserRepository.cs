268925b32af966e346e680eade8a3995404523c0
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-05-31 15:54:23 +1000

    CHQ-1: Added UserRepository.

---
+++ CelebrancyHQ.Repository/Users/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using CelebrancyHQ.Entities;
+
+namespace CelebrancyHQ.Repository.Users
+{
+    /// <summary>
+    /// A users repository.
+    /// </summary>
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Finds the user with the specified email address.
+        /// </summary>
+        /// <param name="emailAddress">The email address of the user.</param>
+        /// <returns>The user with the specified email address.</returns>
+        Task<User?> FindByEmailAddress(string emailAddress);
+    }
+}
---
+++ CelebrancyHQ.Repository/Users/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using CelebrancyHQ.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CelebrancyHQ.Repository.Users
+{
+    /// <summary>
+    /// The users repository.
+    /// </summary>
+    public class UserRepository : IUserRepository
+    {
+        private CelebrancyHQContext _context { get; set; } 
+
+        /// <summary>
+        /// Creates a new instance of UserRepository.
+        /// </summary>
+        /// <param name="context">The database context.</param>
+        public UserRepository(CelebrancyHQContext context)
+        {
+            this._context = context;
+        }
+
+        /// <summary>
+        /// Finds the user with the specified email address.
+        /// </summary>
+        /// <param name="emailAddress">The email address of the user.</param>
+        /// <returns>The user with the specified email address.</returns>
+        public async Task<User?> FindByEmailAddress(string emailAddress)
+        {
+            var user = await this._context.Users.Where(u => u.EmailAddress == emailAddress).FirstOrDefaultAsync();
+
+            return user;
+        }
+    }
+}
