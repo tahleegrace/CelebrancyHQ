a568b6c69c156562daef27ab314b95bc3c04311a
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-06-02 11:08:52 +1000

    CHQ-5: Added CeremonyTypeDTO class.

---
+++ CelebrancyHQ.Models/DTOs/Ceremonies/CeremonyTypeDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace CelebrancyHQ.Models.DTOs.Ceremonies
+{
+    /// <summary>
+    /// A type of ceremony that can be offered (such as a wedding or funeral).
+    /// </summary>
+    public class CeremonyTypeDTO
+    {
+        /// <summary>
+        /// Gets or sets the ID of the ceremony type.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the ceremony that can be offered.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description of the ceremony that can be offered.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether the ceremony can only be offered by the current organisation.
+        /// </summary>
+        public bool RestrictedToOrganisation { get; set; }
+    }
+}
