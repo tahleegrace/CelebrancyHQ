812d12dacf2eedbbf67acc98e926527aaa3502cd
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-06-06 19:48:41 +1000

    CHQ-6: Added navbar to ceremony details page.

--- CelebrancyHQ.UI/src/pages/ceremonies/ceremony-details/ceremony-details.tsx
+++ CelebrancyHQ.UI/src/pages/ceremonies/ceremony-details/ceremony-details.tsx
@@ -21,7 +21,43 @@ class CeremonyDetails extends CommonPage<CeremonyDetailsProps, CeremonyDetailsSt
         return (
             <main>
                 <h1>Ceremony {this.props.params.ceremonyId}</h1>
-                <div>Insert ceremony details content here</div>
+                <div className="container-fluid p-0">
+                    <ul className="nav nav-tabs">
+                        <li className="nav-item">
+                            <a className="nav-link active">Key Details</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">Key Dates</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">Participants</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">Service Providers</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">Order of Service</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">Meetings</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">Documentation</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">Checklists</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">Invoices and Payments</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">History</a>
+                        </li>
+                    </ul>
+                    <div className="container pt-2 pb-0 pl-0 pr-0">
+                        Insert tab content here.
+                    </div>
+                </div>
             </main>
         );
     }
