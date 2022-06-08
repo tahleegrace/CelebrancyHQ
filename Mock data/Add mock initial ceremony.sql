82aaeaca926a65d53ccc61e665ca44cef66b43cb
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-06-08 15:08:06 +1000

    CHQ-6: Added mock witnesses and bridal party members.

--- Mock data/Add mock initial ceremony.sql
+++ Mock data/Add mock initial ceremony.sql
@@ -13,6 +13,20 @@ VALUES ('Jill', 'Biden', 'jillbidentest@celebrancyhq.co', 'F', 'Jill', 'Mrs', '1
 DECLARE @JillBidenId INT
 SET @JillBidenId = SCOPE_IDENTITY()*/
 
+/*INSERT INTO Persons (FirstName, LastName, EmailAddress, Gender, PreferredName, Title, DateOfBirth, Created, Updated)
+VALUES ('Barack', 'Obama', 'barackobamatest@celebrancyhq.co', 'M', 'Barack', 'Mr', '1961-08-04', GETUTCDATE(), GETUTCDATE())*/
+
+DECLARE @BarackObamaId INT
+/*SET @BarackObamaId = SCOPE_IDENTITY()*/
+SET @BarackObamaId = 4
+
+/*INSERT INTO Persons (FirstName, LastName, EmailAddress, Gender, PreferredName, Title, DateOfBirth, Created, Updated)
+VALUES ('Nancy', 'Pelosi', 'nancypelositest@celebrancyhq.co', 'F', 'Nancy', 'Mrs', '1940-03-26', GETUTCDATE(), GETUTCDATE())*/
+
+DECLARE @NancyPelosiId INT
+/*SET @NancyPelosiId = SCOPE_IDENTITY()*/
+SET @NancyPelosiId = 5
+
 /* Mock ceremony. */
 /*INSERT INTO Ceremonies (Name, CeremonyDate, CeremonyTypeId, Created, Updated)
 VALUES ('Marriage of Joseph and Jill Biden', '2022-06-06', 1, GETUTCDATE(), GETUTCDATE())*/
@@ -35,8 +49,22 @@ VALUES (@CeremonyId, @JoeBidenId, 3, GETUTCDATE(), GETUTCDATE())
 INSERT INTO CeremonyParticipants (CeremonyId, PersonId, CeremonyTypeParticipantId, Created, Updated)
 VALUES (@CeremonyId, @JillBidenId, 3, GETUTCDATE(), GETUTCDATE())*/
 
+/* Barack Obama. */
+/*INSERT INTO CeremonyParticipants (CeremonyId, PersonId, CeremonyTypeParticipantId, Created, Updated)
+VALUES (@CeremonyId, @BarackObamaId, 7, GETUTCDATE(), GETUTCDATE())*/
+
+/*INSERT INTO CeremonyParticipants (CeremonyId, PersonId, CeremonyTypeParticipantId, Created, Updated)
+VALUES (@CeremonyId, @BarackObamaId, 6, GETUTCDATE(), GETUTCDATE())*/
+
+/* Nancy Pelosi. */
+/*INSERT INTO CeremonyParticipants (CeremonyId, PersonId, CeremonyTypeParticipantId, Created, Updated)
+VALUES (@CeremonyId, @NancyPelosiId, 7, GETUTCDATE(), GETUTCDATE())*/
+
+/*INSERT INTO CeremonyParticipants (CeremonyId, PersonId, CeremonyTypeParticipantId, Created, Updated)
+VALUES (@CeremonyId, @NancyPelosiId, 6, GETUTCDATE(), GETUTCDATE())*/
+
 /* Mock venue. */
-INSERT INTO Addresses (StreetAddress, Suburb, State, Postcode, Country, Created, Updated, Deleted)
+/*INSERT INTO Addresses (StreetAddress, Suburb, State, Postcode, Country, Created, Updated, Deleted)
 VALUES ('George St', 'Brisbane City', 'QLD', '4000', 'Australia', GETUTCDATE(), GETUTCDATE(), 0)
 
 DECLARE @AddressId INT
@@ -49,4 +77,4 @@ DECLARE @OrganisationId INT
 SET @OrganisationId = SCOPE_IDENTITY()
 
 INSERT INTO CeremonyVenues (CeremonyId, OrganisationId, Name, IsPrimary, Created, Updated, Deleted)
-VALUES (@CeremonyId, @OrganisationId, 'Ceremony venue', 1, GETUTCDATE(), GETUTCDATE(), 0)
+VALUES (@CeremonyId, @OrganisationId, 'Ceremony venue', 1, GETUTCDATE(), GETUTCDATE(), 0)*/
