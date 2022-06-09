976965539eb11e704df81165fa343ff333f12946
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-06-09 23:16:38 +1000

    CHQ-7: Added CelebrancyHQ.Entities.Auditing project.

---
+++ CelebrancyHQ.Entities.Auditing/AuditLog.cs
@@ -0,0 +1,23 @@
+﻿namespace CelebrancyHQ.Entities.Auditing
+{
+    /// <summary>
+    /// An audit log for an entity.
+    /// </summary>
+    public abstract class AuditLog
+    {
+        /// <summary>
+        /// Gets or sets the field name.
+        /// </summary>
+        public abstract string FieldName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the event name.
+        /// </summary>
+        public abstract string EventName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the event data.
+        /// </summary>
+        public abstract Dictionary<string, object> EventData { get; set; }
+    }
+}
